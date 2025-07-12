b58f0da05aaa858efbb4c841eff5646e1675b252
phcreery <phcreery@gmail.com>
2025-07-12 17:26:50 +0000

    fix password change

--- src/layouts/components/Header/components/PasswordDialog.vue
+++ src/layouts/components/Header/components/PasswordDialog.vue
@@ -1,15 +1,5 @@
 <template>
   <el-dialog v-model="dialogVisible" title="Change password" width="500px" draggable>
-    <!-- <el-row :gutter="20">
-      <el-input v-model="oldPassword" type="password" placeholder="Please input old password" show-password />
-    </el-row>
-    <el-row :gutter="20">
-      <el-input v-model="newPassword" type="password" placeholder="Please input new password" show-password />
-    </el-row>
-    <el-row :gutter="20">
-      <el-input v-model="newPasswordConfirm" type="password" placeholder="Please confirm password" show-password />
-    </el-row> -->
-
     <el-form ref="ruleFormRef" :rules="rules" :model="rowData" label-width="130px" label-suffix=" :" :append-to-body="true">
       <el-form-item label="Old Password" prop="oldPassword">
         <el-input v-model="rowData.oldPassword" type="password" placeholder="Please input old password" clearable />
@@ -32,15 +22,12 @@
 
 <script setup lang="ts">
 import { ref, computed, reactive } from "vue";
-// import { useGlobalStore } from "@/stores/modules/global";
 import { useUserStore } from "@/stores/modules/user";
 import { patchUserUpdate } from "@/api/modules/components";
 import { ElNotification, FormInstance } from "element-plus";
+import router from "@/routers";
 
 const dialogVisible = ref(false);
-// const oldPassword = ref("");
-// const newPassword = ref("");
-// const newPasswordConfirm = ref("");
 const userStore = useUserStore();
 const userInfo = computed(() => userStore.userInfo);
 
@@ -57,15 +44,10 @@ const rules = reactive({
   newPasswordConfirm: [{ required: true, message: "Please confirm the new password", trigger: "change" }]
 });
 
-// const passwordsOkay = computed(() => newPassword.value === newPasswordConfirm.value && newPassword.value.length > 8);
-const checkValidate = (val: string) => {
-  ruleFormRef.value!.validateField(val, () => {});
-};
-
 const changePassword = async () => {
   // Validate form
-  await ruleFormRef.value!.validate();
   try {
+    await ruleFormRef.value!.validate();
     const res = await patchUserUpdate({
       ...userInfo.value,
       password: rowData.newPassword,
@@ -83,6 +65,12 @@ const changePassword = async () => {
     rowData.oldPassword = "";
     rowData.newPassword = "";
     rowData.newPasswordConfirm = "";
+
+    // logout
+    userStore.setToken("");
+    router.replace({
+      path: "/login"
+    });
   } catch (error) {
     console.log(error);
     ElNotification({
